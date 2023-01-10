22b5dca41c007145281cc88114017dc6592a58d6
Can Kanbur <cankanbur1@gmail.com>
2023-01-10 20:00:33 +0300

    added nav component

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBk5mRcq0O1HHPlkrZWnuRdt5gDMPgwO4k',
+  authDomain: 'netflix-clone-web-7566c.firebaseapp.com',
+  projectId: 'netflix-clone-web-7566c',
+  storageBucket: 'netflix-clone-web-7566c.appspot.com',
+  messagingSenderId: '533980082810',
+  appId: '1:533980082810:web:94a32244846740396001c3',
+};
